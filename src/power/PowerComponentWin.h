5de79a598433e31793b0bb23f55375b2c7215a82
LongChair <LongChair@hotmail.com>
2016-04-06 13:01:27 +0200

    Add shutdown/suspend/reboot to Windows, part of #283

--- src/power/PowerComponentWin.h
+++ src/power/PowerComponentWin.h
@@ -2,6 +2,7 @@
 #define POWERCOMPONENTWIN_H
 
 #include "PowerComponent.h"
+#include "utils/Utils.h"
 
 class PowerComponentWin : public PowerComponent
 {
@@ -9,6 +10,14 @@ public:
   PowerComponentWin() : PowerComponent(0) { }
   virtual void doDisableScreensaver();
   virtual void doEnableScreensaver();
+
+  virtual bool canPowerOff() override { return WinUtils::getPowerManagementPrivileges(); }
+  virtual bool canReboot() override { return WinUtils::getPowerManagementPrivileges(); }
+  virtual bool canSuspend() override { return true; }
+
+  virtual bool PowerOff() override;
+  virtual bool Reboot() override;
+  virtual bool Suspend() override;
 };
 
 #endif // POWERCOMPONENTWIN_H
