9496fa9e61d1d55966d66ec8476d75b90c9903c4
Rodger Combs <rodger.combs@gmail.com>
2016-11-22 00:21:53 -0600

    Initial support for Bluetooth and Touch Bar controls on macOS
    This uses a private API to keep the Touch Bar controls active in the foreground
    Both iTunes and Safari do the same; Safari source here:
    https://github.com/WebKit/webkit/blob/bde8ac5bddc49527396f3adb576d9a101a8e4828/Source/WebCore/platform/audio/mac/MediaSessionManagerMac.mm#L202

--- src/player/PlayerComponent.cpp
+++ src/player/PlayerComponent.cpp
@@ -443,6 +443,7 @@ void PlayerComponent::updatePlaybackState()
       emit error(m_playbackError);
       break;
     }
+    emit stateChanged(newState, m_state);
     m_state = newState;
   }
 
@@ -1113,6 +1114,9 @@ void PlayerComponent::updateVideoAspectSettings()
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void PlayerComponent::updateVideoSettings()
 {
+  if (!m_mpv)
+    return;
+
   QVariant syncMode = SettingsComponent::Get().value(SETTINGS_SECTION_VIDEO, "sync_mode");
   mpv::qt::set_property(m_mpv, "video-sync", syncMode);
 
