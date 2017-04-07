1a13087c5e27d7dcff9fe03781ea560c634d44f5
Vincent Lang <vincent@plexapp.com>
2017-04-07 15:20:09 +0200

    will it work?

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -7,4 +7,4 @@ if exist ../conanfile.py (
 %CMAKE_DIR%\cmake -DDEPENDENCY_TOKEN=%DEP_SECRET% -DCRASHDUMP_SECRET=%CD_SECRET% -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=output -DDEPENDENCY_UNTAR_DIR=c:\jenkins\pmp-deps .. -G Ninja -DCODE_SIGN=ON -DENABLE_CODECS=on || exit /b
 
 ninja || exit /b
-ninja windows_package || exit /b
+ninja package || exit /b
