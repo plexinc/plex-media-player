4964a41f9a9b045409cffff0d176c9e2814d02d7
Vincent Lang <vincent@plexapp.com>
2017-06-14 12:55:36 +0200

    KonvergoWindow: strictly ignore forceFSScreen if it's empty
    In theory, QScreen::name() could return an empty string. Then this could
    would do something, even if the forceFSScreen setting is disabled.
    
    Check it explicitly to avoid this. This also means the setting won't
    work if a screen actually has an empty name, but such is life.

--- src/ui/KonvergoWindow.cpp
+++ src/ui/KonvergoWindow.cpp
@@ -436,6 +436,9 @@ void KonvergoWindow::updateForcedScreen()
 {
   QString screenName = SettingsComponent::Get().value(SETTINGS_SECTION_MAIN, "forceFSScreen").toString();
 
+  if (screenName.isEmpty())
+    return;
+
   for (QScreen* scr : QGuiApplication::screens())
   {
     if (scr->name() == screenName)
